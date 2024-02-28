a83ead463b4358924b47edbe5db661e4066c5db5
htriki1204 <htriki@ucm.es>
2024-02-28 16:44:58 +0100

    Limpieza Main

--- src/2.limpieza/main.py
+++ src/2.limpieza/main.py
@@ -1,3 +1,20 @@
 import limpieza
+import pandas as pd
+import numpy as np
+
+# Lee el archivo CSV y almacena su contenido en un DataFrame
+url = '//Users//hamzatriki//Downloads//Casas_todas'
+datos = pd.read_csv(url,usecols = lambda columna: columna!= "Unnamed: 0")
+
+# Muestra las primeras filas del DataFrame para verificar que se ha leído correctamente
+print(datos.head())
+
+ColumnasModificar = ["Dormitorios","Superficie","Num_baños","Precio"]
+for columna in ColumnasModificar:
+    datos[columna] = datos[columna].replace("No disponible",np.nan)
+    datos[columna] = datos[columna].apply(limpieza.string_to_int())
+datos["Tipo"] = datos["Enlace"].apply(limpieza.obtener_tipo_casa())
+datos = datos.drop(columns = ["Enlace"])
+
 
 
