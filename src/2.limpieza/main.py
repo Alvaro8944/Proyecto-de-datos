efc15cc6c6f78c2abff3ea6600788877bf746386
htriki1204 <htriki@ucm.es>
2024-03-03 11:44:43 +0100

    Limpieza Main

--- src/2.limpieza/main.py
+++ src/2.limpieza/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # Lee el archivo CSV y almacena su contenido en un DataFrame
-url = '/Users/hamzatriki/Casas_todas'
+url = '/Users/hamzatriki/DatosCasas'
 datos = pd.read_csv(url,usecols = lambda columna: columna!= "Unnamed: 0")
 
 # Muestra las primeras filas del DataFrame para verificar que se ha leído correctamente
@@ -16,20 +16,28 @@ for columna in ColumnasModificar:
 datos["Tipo"] = datos["Enlace"].apply(limpieza.obtener_tipo_casa)
 datos = datos.drop(columns = ["Enlace"])
 datos["Precio"] = datos["Precio"].apply(limpieza.string_to_price)
-
+## transformar las variables [Calefaccion","Ascensor","Aire acondicionado","Jardin"] en dicotómicas
 datos = limpieza.transformar_en_dicotomicas(datos)
-""" 
-probar
-dataframe_descripcion = datos["Descripción"].apply(limpieza.analizar_descripcion())
-columnas =["Ascensor", "Dormitorios","Num_baños"]
+## analizar la descripción
+dataframe_descripcion = datos["Descripción"].apply(limpieza.analizar_descripcion)
+columnas =["Ascensor", "Dormitorios","Num_baños","Año_de_construccion"]
 dataframe_descripcion = pd.DataFrame(dataframe_descripcion.tolist(), columns = columnas)
 for columna in columnas[1:]:
     datos[columna] = datos[columna].fillna(dataframe_descripcion[columna])
 datos["Ascensor"] = datos["Ascensor"].combine(dataframe_descripcion["Ascensor"],max)
-#urls = []
-#limpieza.transformar_localizacion(datos,urls)
-"""
-print(datos)
+datos.loc[datos["Etiqueta"].isna(),"Etiqueta"] = "En proceso"
+
+# obtener el distrito de la vivienda
+urls = ["/Users/hamzatriki/Downloads/barrios_municipio_madrid.csv","/Users/hamzatriki/Downloads/municipio_comunidad_madrid.csv"]
+limpieza.transformar_localizacion(datos,urls)
+
+# Nos quedamos solo con los viviendas de tipo "casa","piso"y "ático
+viviendas = ["casa","piso","atico"]
+filtro_viviendas = datos["Tipo"].isin(viviendas)
+datos_viviendas = datos[filtro_viviendas]
+datos_viviendas.info()
+
+
 
 
 
