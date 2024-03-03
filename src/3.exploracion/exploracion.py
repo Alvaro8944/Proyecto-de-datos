573a79ecc7f33794e701425e90174ce5d98194cb
diegal04 <diegal04@ucm.es>
2024-03-03 19:09:16 +0100

    Exploracion (correlacion_correcion)

--- src/3.exploracion/exploracion.py
+++ src/3.exploracion/exploracion.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def correlacion(data):
-    correlation_matrix = df.corr()
+    correlation_matrix = data.corr()
     correlation_matrix
     plt.figure(figsize=(10, 8))
     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
